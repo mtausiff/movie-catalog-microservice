ddb1dae92dce33abf4f0a2a79cc888945b77cda9
mtausiff <tausifcse17@gmail.com>
2019-12-15 20:35:43 +0300

    Initial movie-catalog-microservice project commit

---
+++ movie-catalog-microservice/src/main/java/com/movie/catalog/microservice/MovieCatalogMicroserviceApplication.java
@@ -0,0 +1,13 @@
+package com.movie.catalog.microservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MovieCatalogMicroserviceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MovieCatalogMicroserviceApplication.class, args);
+	}
+
+}
---
+++ movie-catalog-microservice/src/test/java/com/movie/catalog/microservice/MovieCatalogMicroserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.movie.catalog.microservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MovieCatalogMicroserviceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
